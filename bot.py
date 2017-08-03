647db18773133e7bb84878886020fcc6568712d1
Ecophagy <olly.spoon@gmail.com>
2017-08-03 22:03:22 +0100

    Switched to discord.ext commands format

--- bot.py
+++ bot.py
@@ -1,36 +1,42 @@
 import discord
+from discord.ext import commands
 import asyncio
 import json
 
-client = discord.Client()
+description = '''Bot for receiving diplomacy commands'''
+bot = commands.Bot(command_prefix='!', description=description)
 
-@client.event
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        if message.channel.is_private:
-            counter = 0
-            tmp = await client.send_message(message.channel, 'Calculating messages...')
-            async for log in client.logs_from(message.channel, limit=100):
-                if log.author == message.author:
-                    counter += 1
 
-            await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-        else:
-            await client.send_message(message.channel, 'I only test in private')
+@bot.command()
+async def sleep():
+    """Sleep for 5 seconds"""
+    await asyncio.sleep(5)
+    await bot.say('Done sleeping')
 
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+
+@bot.command(pass_context=True)
+async def test(ctx):
+    if ctx.message.channel.is_private:
+        counter = 0
+        tmp = await bot.say('Calculating messages...')
+        async for log in bot.logs_from(ctx.message.channel, limit=100):
+            if log.author == ctx.message.author:
+                counter += 1
+
+        await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
+    else:
+        await bot.say('I only test in private')
 
 
 with open('config.json') as data_file:
     data = json.load(data_file)
 
-client.run(data["token"])
+bot.run(data["token"])
